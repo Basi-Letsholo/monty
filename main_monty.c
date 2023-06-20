3a59a9fb02de5e8419a3267577a8a9d4b839651e
Basi-Letsholo <basi.letsholo7@gmail.com>
2023-06-20 09:01:38 -0700

    changes from webterm

---
+++ main_monty.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+int main(int ac, char **av)
+{
+	/* init vars */
+	(void)av;
+
+	if (ac != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	return (0);
+	/* change later?? */
+}
